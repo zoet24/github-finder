eb20b1bf64b816d0f84245350492c4b7d04a1f12
Zoe <zt12542@my.bristol.ac.uk>
2023-03-21 20:43:42 +0000

    Add repo components

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -59,9 +60,31 @@ export const GithubProvider = ({ children }) => {
         }
     }
 
+    // Get user repos
+    const getUserRepos = async (login) => {
+        setLoading()
+
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
+    }
+
     // Clear search results
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
-
 
     // Set loading
     const setLoading = () => dispatch({ type: 'SET_LOADING' })
@@ -70,9 +93,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
